ccca38888d82537e4ebaed41ee4d44ecd622895e
DarcyMerrick <70248092+DarcyMerrick@users.noreply.github.com>
2022-06-19 20:56:20 +1000

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,7 +4,7 @@ import Hero from '../components/hero/hero';
 const Home = () => {
   return (
     <div>
-      <p className="banner">This website is a work in progress. I plan to make contributions multiple times per week but is still very much a slow burner</p>
+      <p className="banner">This website is a work in progress. I plan to make contributions multiple times per week but is still very much a slow burner.</p>
       <Hero title="Darcy Merrick" subtitle="A web developer" />
     </div>
   );
