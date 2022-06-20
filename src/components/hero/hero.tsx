10db9385d08b1da4a62ac8a5da14d07d0b30e80d
Darcy Merrick <darcy.e.merrick@gmail.com>
2022-06-20 20:13:22 +1000

    Added some links to the home page

--- src/components/hero/hero.tsx
+++ src/components/hero/hero.tsx
@@ -1,11 +1,12 @@
+import Container from '../container/container';
 import './hero.scss'
 
 const Hero = ({title, subtitle}:any) => {
     return (
-        <div className='hero'>
+        <Container className='hero'>
             <h1 className='hero-title'>{title}</h1>
             <h2 className='hero-subtitle'>{subtitle}</h2>
-        </div>
+        </Container>
     );
 }
 
